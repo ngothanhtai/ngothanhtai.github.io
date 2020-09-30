9eb37cb25135ea8b6d4e37d64a94d055d4d5774f
Walter Ngo <codesigngo@gmail.com>
2020-09-30 19:26:51 +0700

    chore: rename build to docs

---
+++ docs/precache-manifest.a76dd808901166f980784a3f90e088c1.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c31a1ddd8dfd8173ae67605aafd6b1a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e784bcb877a6cc947f24",
+    "url": "/static/css/main.c1f86bb3.chunk.css"
+  },
+  {
+    "revision": "bd06ed97bb45696ed211",
+    "url": "/static/js/2.6f8cba21.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.6f8cba21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e784bcb877a6cc947f24",
+    "url": "/static/js/main.dafa45a5.chunk.js"
+  },
+  {
+    "revision": "093d07080cb5689b4eca",
+    "url": "/static/js/runtime-main.aa55c731.js"
+  },
+  {
+    "revision": "0f3514a45d51f95167e5fe8b6a03bb60",
+    "url": "/static/media/appstore.0f3514a4.svg"
+  },
+  {
+    "revision": "8b2e00cf21674367534911d8e1b1179b",
+    "url": "/static/media/click.8b2e00cf.wav"
+  },
+  {
+    "revision": "1c4dd1cbfe426c32e7e017e80890a479",
+    "url": "/static/media/display-connected-wifi-name-screenshot.1c4dd1cb.png"
+  },
+  {
+    "revision": "3e54ed15b9cd877c5223f5ecf64579df",
+    "url": "/static/media/github.3e54ed15.png"
+  },
+  {
+    "revision": "1290c17dcab8919875d080fecc904e0e",
+    "url": "/static/media/mac-app-store.1290c17d.svg"
+  },
+  {
+    "revision": "9880dd46c3703e120411bbeb818f8b16",
+    "url": "/static/media/mgl-screenshot.9880dd46.png"
+  },
+  {
+    "revision": "fcc2e055299cc771d2f1bb8ef46da3fb",
+    "url": "/static/media/scroll-to-top-screenshot.fcc2e055.png"
+  },
+  {
+    "revision": "4587b43342028c36fb6fa75d35fefc19",
+    "url": "/static/media/walter.4587b433.jpeg"
+  },
+  {
+    "revision": "8a99f5c3fb12c7f0728a7175126c3ca9",
+    "url": "/static/media/walterscc-screenshot.8a99f5c3.png"
+  }
+]);
