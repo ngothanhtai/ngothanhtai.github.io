b96a0ae98b7abfb480efdb674eb29339f7574838
Walter Ngo <codesigngo@gmail.com>
2020-10-01 09:35:38 +0700

    chore: table of contents

--- src/components/TableOfContents/TableOfContents.tsx
+++ src/components/TableOfContents/TableOfContents.tsx
@@ -1,99 +1,69 @@
 import React from "react";
 import { HashUrl } from "../../utils/constants";
+import "./TableOfContents.css";
 
+const tableOfContentItems = [
+  {
+    hashUrl: HashUrl.summary,
+    icon: "📃",
+    title: "Summary",
+  },
+  {
+    hashUrl: HashUrl.skills,
+    icon: "🛠",
+    title: "Skills",
+  },
+  {
+    hashUrl: HashUrl.sideProjects,
+    icon: "🪓",
+    title: "Side Projects",
+  },
+  {
+    hashUrl: HashUrl.workExperience,
+    icon: "💼",
+    title: "Work Experience",
+  },
+  {
+    hashUrl: HashUrl.languages,
+    icon: "🗣",
+    title: "Languages",
+  },
+  {
+    hashUrl: HashUrl.publication,
+    icon: "📜",
+    title: "Publication",
+  },
+  {
+    hashUrl: HashUrl.awards,
+    icon: "🏆",
+    title: "Awards",
+  },
+  {
+    hashUrl: HashUrl.education,
+    icon: "📚",
+    title: "Education",
+  },
+  {
+    hashUrl: HashUrl.volunteerExperience,
+    icon: "✋",
+    title: "Volunteer Experience",
+  },
+];
 const TableOfContents: React.FC = () => {
   return (
-    <ul>
-      <li>
-        <a
-          href={`#${HashUrl.summary}`}
-          className="textDecorationNone highlightOnHover"
-        >
-          <span role="img" aria-label="Summary">
-            📃 Summary
-          </span>
-        </a>
-      </li>
-      <li>
-        <a
-          href={`#${HashUrl.skills}`}
-          className="textDecorationNone highlightOnHover"
-        >
-          <span role="img" aria-label="Skills">
-            🛠 Skills
-          </span>
-        </a>
-      </li>
-      <li>
-        <a
-          href={`#${HashUrl.sideProjects}`}
-          className="textDecorationNone highlightOnHover"
-        >
-          <span role="img" aria-label="Side Projects">
-            🪓 Side Projects
-          </span>
-        </a>
-      </li>
-      <li>
-        <a
-          href={`#${HashUrl.workExperience}`}
-          className="textDecorationNone highlightOnHover"
-        >
-          <span role="img" aria-label="Work Experience">
-            💼 Work Experience
-          </span>
-        </a>
-      </li>
-      <li>
-        <a
-          href={`#${HashUrl.languages}`}
-          className="textDecorationNone highlightOnHover"
-        >
-          <span role="img" aria-label="Languages">
-            🗣 Languages
-          </span>
-        </a>
-      </li>
-      <li>
-        <a
-          href={`#${HashUrl.publication}`}
-          className="textDecorationNone highlightOnHover"
-        >
-          <span role="img" aria-label="Publication">
-            📜 Publication
-          </span>
-        </a>
-      </li>
-      <li>
-        <a
-          href={`#${HashUrl.awards}`}
-          className="textDecorationNone highlightOnHover"
-        >
-          <span role="img" aria-label="Awards">
-            🏆 Awards
-          </span>
-        </a>
-      </li>
-      <li>
-        <a
-          href={`#${HashUrl.education}`}
-          className="textDecorationNone highlightOnHover"
-        >
-          <span role="img" aria-label="Education">
-            📚 Education
-          </span>
-        </a>
-      </li>
-      <li>
-        <a
-          href={`#${HashUrl.volunteerExperience}`}
-          className="textDecorationNone highlightOnHover"
-        >
-          <span role="img" aria-label="Volunteer Experience">
-            ✋ Volunteer Experience
-          </span>
-        </a>
-      </li>
+    <ul className="tableOfContents">
+      {tableOfContentItems.map(({ hashUrl, icon, title }) => (
+        <li key={hashUrl}>
+          <a
+            href={`#${hashUrl}`}
+            className="textDecorationNone highlightOnHover"
+          >
+            <span role="img" aria-label={title}>
+              {icon} {title}
+            </span>
+          </a>
+        </li>
+      ))}
     </ul>
   );
 };
